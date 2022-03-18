00c83c099b88b5ef36013e2e89112c9d7cd27ebe
Max Cardwell <maximillian.cardwell@gmail.com>
2022-03-18 21:49:35 +0000

    Add camera and page to see individual camera is working

--- console.py
+++ console.py
@@ -6,22 +6,36 @@ from models.make import Make
 import repositories.camera_repository as camera_repository
 import repositories.make_repository as make_repository
 
-camera_repository.delete_all()
-make_repository.delete_all()
 
-make1 = Make("Canon")
-make2 = Make("Nikon")
-make3 = Make("Sony")
+camera = camera_repository.select(14)
+print(camera)
 
-make_repository.save(make1)
-make_repository.save(make2)
-make_repository.save(make3)
+# camera_repository.delete_all()
+# make_repository.delete_all()
 
-camera1 = Camera("250d", make1, "Compact DSLR", "Lightweight APS-C mid-range camera", 5, 250, 500)
-camera2 = Camera("a7", make3, "Mirrorless Camera", "Full frame high end camera", 3, 600, 1200)
+# make_canon = Make("Canon")
+# make_nikon = Make("Nikon")
+# make_sony = Make("Sony")
+# make_fuji = Make("Fuji")
+# make_panasonic = Make("Panasonic")
+# make_olympus = Make("Olympus")
 
-camera_repository.save(camera1)
-camera_repository.save(camera2)
+# make_repository.save(make_canon)
+# make_repository.save(make_nikon)
+# make_repository.save(make_sony)
+# make_repository.save(make_fuji)
+# make_repository.save(make_panasonic)
+# make_repository.save(make_olympus)
+
+# camera_compact_dslr = Camera("250d", make_canon, "Compact DSLR", "Lightweight APS-C DSLR mid-range camera", 5, 250, 500)
+# camera_mirroless = Camera("a7", make_sony, "Mirrorless Camera", "Full frame mirrorless high end camera", 3, 600, 1200)
+# camera_compact_mirroless = Camera("M50", make_canon, "Compact Mirroless", "Lightweight APS-C mirrorless mid-range camera", 4, 300, 600)
+# camera_dslr = Camera("5D MKIV", make_canon, "DSLR", "Full frame DSLR high end camera", 2, 1000, 2000)
+
+# camera_repository.save(camera_dslr)
+# camera_repository.save(camera_compact_dslr)
+# camera_repository.save(camera_mirroless)
+# camera_repository.save(camera_compact_mirroless)
 
 # camera = camera_repository.select(2)
 # print(camera)
@@ -30,14 +44,14 @@ camera_repository.save(camera2)
 
 # camera_repository.delete(10)
 
-cameras = camera_repository.select_all()
-for camera in cameras:
-    print(camera.__dict__)
+# cameras = camera_repository.select_all()
+# for camera in cameras:
+#     print(camera.__dict__)
 
 
-makes = make_repository.select_all()
-for make in makes:
-    print(make.__dict__)
+# makes = make_repository.select_all()
+# for make in makes:
+#     print(make.__dict__)
 
 # selected_make = make_repository.select(3)
 # print(selected_make)
--- controllers/cameras__controller.py
+++ controllers/cameras__controller.py
@@ -17,4 +17,40 @@ def cameras():
 @cameras_blueprint.route('/cameras/new', methods=['GET'])
 def new_camera():
     makes = make_repository.select_all()
-    return render_template('cameras/new.html', makes=makes)
+    cameras = camera_repository.select_all()
+    return render_template('cameras/new.html', makes=makes, cameras=cameras)
+
+@cameras_blueprint.route("/cameras", methods=['POST'])
+def add_camera():
+    stock = 0
+    stock_count_form = request.form['stock']
+    buy_price = 0
+    buy_price_form = request.form['buy_price']
+    sell_price = 0
+    sell_price_form = request.form['sell_price']
+
+    name = request.form['name']
+    make_id = request.form['make_id']
+    type = request.form['type']
+    description = request.form['description']
+
+    if stock_count_form is not None:
+        stock = stock_count_form
+
+    if buy_price_form is not None:
+        buy_price = buy_price_form
+
+    if sell_price_form is not None:
+        sell_price = sell_price_form
+
+    make = make_repository.select(make_id)
+    camera = Camera(name, make, type, description, stock, buy_price, sell_price)
+
+    camera_repository.save(camera)
+    return redirect('/cameras')
+
+
+@cameras_blueprint.route("/cameras/<id>", methods=['GET'])
+def show(id):
+    camera = camera_repository.select(id)
+    return render_template('cameras/show.html', camera=camera)
