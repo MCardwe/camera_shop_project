2312834686386da65b575ac41800d9b47ad64b2a
Max Cardwell <maximillian.cardwell@gmail.com>
2022-03-18 15:29:25 +0000

    Created and tested save and select all function from make_repository

--- console.py
+++ console.py
@@ -6,5 +6,14 @@ from models.make import Make
 import repositories.camera_repository as camera_repository
 import repositories.make_repository as make_repository
 
+make1 = Make("Canon")
+make2 = Make("Nikon")
+
+make_repository.save(make1)
+make_repository.save(make2)
+
+makes = make_repository.select_all()
+for make in makes:
+    print(make.__dict__)
 
 pdb.set_trace()
--- repositories/make_repository.py
+++ repositories/make_repository.py
@@ -1,4 +1,42 @@
+from unicodedata import name
 from db.run_sql import run_sql
 
 from models.camera import Camera
 from models.make import Make
+
+
+def save(make):
+    
+    sql = "INSERT INTO makes (name) VALUES (%s) RETURNING *"
+    values = [make.name]
+
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    make.id = id
+
+    return make
+
+def select_all():
+    makes = []
+
+    sql = "SELECT * FROM makes"
+
+    results = run_sql(sql)
+
+    for row in results:
+        make = Make(row['name'], row['id'])
+        makes.append(make)
+
+    return makes
+
+def select():
+    pass
+
+def update():
+    pass
+
+def delete_all():
+    pass
+
+def delete():
+    pass
