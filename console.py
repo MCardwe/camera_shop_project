21ff92ae80223b85be0af0e956dc0d91484244c8
Max Cardwell <maximillian.cardwell@gmail.com>
2022-03-18 17:13:35 +0000

    created and tested then fixed save and select_all function for camera repo

--- console.py
+++ console.py
@@ -8,31 +8,39 @@ import repositories.make_repository as make_repository
 
 # make_repository.delete_all()
 
-# make1 = Make("Canon")
-# make2 = Make("Nikon")
+make1 = Make("Canon")
+make2 = Make("Nikon")
+make3 = Make("Sony")
 
 # make_repository.save(make1)
 # make_repository.save(make2)
+# make_repository.save(make3)
 
-makes = make_repository.select_all()
-for make in makes:
-    print(make.__dict__)
+camera1 = Camera("250d", make1, "Compact DSLR", "Lightweight APS-C mid-range camera", 5, 250, 500)
+camera2 = Camera("a7", make3, "Mirrorless Camera", "Full frame high end camera", 3, 600, 1200)
 
-selected_make = make_repository.select(3)
-print(selected_make)
+# camera_repository.save(camera1)
+# camera_repository.save(camera2)
 
-make_sony = Make("Sony", 4)
+cameras = camera_repository.select_all()
+for camera in cameras:
+    print(camera.__dict__)
 
-make_repository.update(make_sony)
+# makes = make_repository.select_all()
+# for make in makes:
+#     print(make.__dict__)
 
-makes = make_repository.select_all()
-for make in makes:
-    print(make.__dict__)
+# selected_make = make_repository.select(3)
+# print(selected_make)
 
-make_repository.delete(4)
+# make_sony = Make("test", 4)
 
-makes = make_repository.select_all()
-for make in makes:
-    print(make.__dict__)
+# make_repository.update(make_sony)
+
+# makes = make_repository.select_all()
+# for make in makes:
+#     print(make.__dict__)
+
+# make_repository.delete(4)
 
 pdb.set_trace()
--- repositories/camera_repository.py
+++ repositories/camera_repository.py
@@ -4,3 +4,28 @@ from models.camera import Camera
 from models.make import Make
 
 import repositories.make_repository as make_repository
+
+def save(camera):
+    
+    sql = "INSERT INTO cameras (name, make_id, type, description, stock, buy_price, sell_price) VALUES (%s, %s, %s, %s, %s, %s, %s) RETURNING *"
+    values = [camera.name, camera.make.id, camera.type, camera.description, camera.stock, camera.buy_price, camera.sell_price]
+
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    camera.id = id
+
+    return camera
+
+def select_all():
+    cameras = []
+
+    sql = "SELECT * FROM cameras"
+
+    results = run_sql(sql)
+
+    for row in results:
+        make = make_repository.select(row['make_id'])
+        camera = Camera(row['name'], make, row['type'], row['description'], row['stock'], row['buy_price'], row['sell_price'], row['id'])
+        cameras.append(camera)
+    
+    return cameras
