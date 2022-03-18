daa5007a50b99878c2c1064da62c81d73933e893
Max Cardwell <maximillian.cardwell@gmail.com>
2022-03-18 20:17:12 +0000

    Made inventory page show list of all cameras

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.register_blueprint(makes_blueprint)
 app.register_blueprint(cameras_blueprint)
 
-app.route("/")
+@app.route("/")
 def home():
     return render_template("index.html")
 
--- controllers/cameras__controller.py
+++ controllers/cameras__controller.py
@@ -8,3 +8,13 @@ import repositories.camera_repository as camera_repository
 import repositories.make_repository as make_repository
 
 cameras_blueprint = Blueprint("cameras", __name__)
+
+@cameras_blueprint.route('/cameras', methods = ['GET'])
+def cameras():
+    cameras = camera_repository.select_all()
+    return render_template('cameras/index.html', cameras=cameras)
+
+@cameras_blueprint.route('/cameras/new', methods=['GET'])
+def new_camera():
+    makes = make_repository.select_all()
+    return render_template('cameras/new.html', makes=makes)
