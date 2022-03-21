0ce0943a270304d43547a6602be5b5205b13ca74
Max Cardwell <maximillian.cardwell@gmail.com>
2022-03-21 13:49:14 +0000

    Fixed bug with stock count

--- console.py
+++ console.py
@@ -10,32 +10,32 @@ import repositories.make_repository as make_repository
 # camera = camera_repository.select(14)
 # print(camera)
 
-# camera_repository.delete_all()
-# make_repository.delete_all()
+camera_repository.delete_all()
+make_repository.delete_all()
 
-# make_canon = Make("Canon")
-# make_nikon = Make("Nikon")
-# make_sony = Make("Sony")
-# make_fuji = Make("Fuji")
-# make_panasonic = Make("Panasonic")
-# make_olympus = Make("Olympus")
+make_canon = Make("Canon")
+make_nikon = Make("Nikon")
+make_sony = Make("Sony")
+make_fuji = Make("Fuji")
+make_panasonic = Make("Panasonic")
+make_olympus = Make("Olympus")
 
-# make_repository.save(make_canon)
-# make_repository.save(make_nikon)
-# make_repository.save(make_sony)
-# make_repository.save(make_fuji)
-# make_repository.save(make_panasonic)
-# make_repository.save(make_olympus)
+make_repository.save(make_canon)
+make_repository.save(make_nikon)
+make_repository.save(make_sony)
+make_repository.save(make_fuji)
+make_repository.save(make_panasonic)
+make_repository.save(make_olympus)
 
-# camera_compact_dslr = Camera("250d", make_canon, "Compact DSLR", "Lightweight APS-C DSLR mid-range camera", 5, 250, 500)
-# camera_mirroless = Camera("a7", make_sony, "Mirrorless Camera", "Full frame mirrorless high end camera", 3, 600, 1200)
-# camera_compact_mirroless = Camera("M50", make_canon, "Compact Mirroless", "Lightweight APS-C mirrorless mid-range camera", 4, 300, 600)
-# camera_dslr = Camera("5D MKIV", make_canon, "DSLR", "Full frame DSLR high end camera", 2, 1000, 2000)
+camera_compact_dslr = Camera("250d", make_canon, "Compact DSLR", "Lightweight APS-C DSLR mid-range camera", 5, 250, 500)
+camera_mirroless = Camera("a7", make_sony, "Mirrorless Camera", "Full frame mirrorless high end camera", 3, 600, 1200)
+camera_compact_mirroless = Camera("M50", make_canon, "Compact Mirroless", "Lightweight APS-C mirrorless mid-range camera", 4, 300, 600)
+camera_dslr = Camera("5D MKIV", make_canon, "DSLR", "Full frame DSLR high end camera", 2, 1000, 2000)
 
-# camera_repository.save(camera_dslr)
-# camera_repository.save(camera_compact_dslr)
-# camera_repository.save(camera_mirroless)
-# camera_repository.save(camera_compact_mirroless)
+camera_repository.save(camera_dslr)
+camera_repository.save(camera_compact_dslr)
+camera_repository.save(camera_mirroless)
+camera_repository.save(camera_compact_mirroless)
 
 # camera = camera_repository.select(2)
 # print(camera)
@@ -44,14 +44,14 @@ import repositories.make_repository as make_repository
 
 # camera_repository.delete(10)
 
-# cameras = camera_repository.select_all()
-# for camera in cameras:
-#     print(camera.__dict__)
+cameras = camera_repository.select_all()
+for camera in cameras:
+    print(camera.__dict__)
 
 
-# makes = make_repository.select_all()
-# for make in makes:
-#     print(make.__dict__)
+makes = make_repository.select_all()
+for make in makes:
+    print(make.__dict__)
 
 # selected_make = make_repository.select(3)
 # print(selected_make)
